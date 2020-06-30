eac16177afeb8b82e2e259dc9df0292f13aee6c7
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-06-29 21:13:08 -0300

    refact layout action on screen list

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Routes from './src/routes';
+import { StatusBar, setStatusBarStyle } from 'expo-status-bar';
+
+setStatusBarStyle('light')
 
 export default function App() {
   return (
--- src/routes.tsx
+++ src/routes.tsx
@@ -8,12 +8,23 @@ import Register from './pages/Register/index';
 
 const Tab = createBottomTabNavigator();
 
+
+const tabBarOptions = {
+  tabStyle: { backgroundColor: '#151728' },
+  style: { borderTopWidth: 0, borderTopColor: "transparent" },
+  showLabel: false,
+  keyboardHidesTabBar: true,
+  activeTintColor: '#1b86f9',
+  inactiveTintColor: '#9c9cab'
+}
+
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="plus-circle" color={color} /> }} name="New" component={Register} />
-        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="list" color={color} /> }} name="List" component={List} />
+      <Tab.Navigator tabBarOptions={tabBarOptions}>
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="plus-circle" size={24} color={color} /> }} name="New" component={Register} />
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="list" size={24} color={color} /> }} name="List" component={List} />
+        {/* <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="settings" size={24} color={color} /> }} name="Settings" component={List} /> */}
         {/* <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="camera" color={color}/> }} name="Camera" component={Camera} /> */}
       </Tab.Navigator>
     </NavigationContainer>
