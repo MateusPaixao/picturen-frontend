74ef9de2d37d821f4903b691f868f7bd3c667d1b
Mateus Paixão <mateus.borges.paixao@gmail.com>
2021-04-09 02:31:50 -0300

    update api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     // baseURL: 'http://192.168.0.3:3000'
-    baseURL: 'https://paixaodev.com.br/picturen'
+    // baseURL: 'https://paixaodev.com.br/picturen' // TODO: fix https issue
+    baseURL: 'http://54.205.251.255/picturen'
 })
 
 export default api
